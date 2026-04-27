6eaf93852d8f29989ee5df4d2364d3c868216586
ramjananshari241 <ramjananshari241@gmail.com>
2026-04-27 23:32:41 +0800

    优化

--- next.config.js
+++ next.config.js
@@ -6,13 +6,38 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'www.notion.so',
       },
+      // Notion-hosted static assets / files
       {
         protocol: 'https',
-        hostname: '*.googleusercontent.com', // accept all subdomains of googleusercontent.com
+        hostname: 'notion.so',
+      },
+      {
+        protocol: 'https',
+        hostname: 'secure.notion-static.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh4.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh5.googleusercontent.com',
       },
       {
         protocol: 'https',
         hostname: 's3-us-west-2.amazonaws.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 's3.us-west-2.amazonaws.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'prod-files-secure.s3.us-west-2.amazonaws.com',
       },
     ],
   },
