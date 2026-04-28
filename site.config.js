8632978a973670a49c624401bdb804ef9de43fc5
ramjananshari241 <ramjananshari241@gmail.com>
2026-04-29 05:51:26 +0800

    优化

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "morethanmin",
-    image: "/https://x1image.top/1764328232935.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/blog.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
     bio: "I develop everything using node.",
     email: "morethanmin.dev@gmail.com",
